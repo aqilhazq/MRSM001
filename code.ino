b231a44750b5af1307c105d33748ca3792dc5c8b
aqilhazq <87805368+aqilhazq@users.noreply.github.com>
2024-05-06 23:44:11 +0800

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,16 +1,24 @@
+#include <Servo.h>
+
 #define left_motor_positive 7
 #define left_motor_negative 6
 #define right_motor_positive 4
 #define right_motor_negative 5
+#define servo 8
 
 int control_left = 2;
 int control_right = 3;
+
+int angle = 0;
+Servo Servo;
 
 void setup() {
   pinMode(left_motor_positive, OUTPUT);
   pinMode(left_motor_negative, OUTPUT);
   pinMode(right_motor_positive, OUTPUT);
   pinMode(right_motor_negative, OUTPUT);
+  Servo.attach(servo);
+  Servo.write(angle);
 }
 
 void move(){
@@ -28,14 +36,14 @@ void move_forward(){
 
 void move_left(){
   move();
-  analogWrite(control_left, 100);
+  analogWrite(control_left, 150);
   analogWrite(control_right, 200);
 }
 
 void move_right(){
   move();
   analogWrite(control_left, 200);
-  analogWrite(control_right, 100);
+  analogWrite(control_right, 150);
 }
 
 void stop(){
@@ -44,4 +52,19 @@ void stop(){
   analogWrite(control_right, 0);
 }
 
-void loop() {}
+void servo_down() {
+  Servo.write(90);
+}
+
+void servo_up() {
+  Servo.write(0);
+}
+
+void loop(){
+  move_forward();
+  delay(5000);
+  stop();
+  servo_down();
+  delay(1000);
+  servo_up();
+}
